bc40e8a2471143eee9e01b173db42037776d7a25
Maya Stucky <mayaatstucky@gmail.com>
2020-08-05 09:57:59 -0400

    almost got portfolio block component working right

--- src/components/PortfolioBlock.jsx
+++ src/components/PortfolioBlock.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
 
-const PortfolioBlock = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const PortfolioBlock = (props) => {
+  const styles = {
+    imageSize: {
+      width: "400px",
+      height: "300px",
+    },
+  };
+
+  return (
+    <div className="col-sm">
+      <figure className="figure">
+        <img
+          src={props.src}
+          className="figure-img img-fluid rounded"
+          alt={props.alt}
+          style={styles.imageSize}
+        />
+        <figcaption className="figure-caption" style={{ marginBottom: "30px" }}>
+          {props.name}
+          <a href={props.deployedLink}>Site</a>
+          <a href={props.githubLink}>GitHub</a>
+        </figcaption>
+      </figure>
+    </div>
+  );
+};
 
 export default PortfolioBlock;
