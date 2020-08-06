4937244fed740a225a82f4ea6c786ab6f8375c00
Maya Stucky <mayaatstucky@gmail.com>
2020-08-06 09:53:30 -0400

    tried to fix ghpages problem

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import Portfolio from "./components/pages/Portfolio";
 import Footer from "./components/Footer";
 
-
-
 function App() {
-
   return (
     <div className="App">
       <Router>
         <Header />
+        <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/mayastucky-portfolio" component={Home} />
         <Route path="/contact" component={Contact} />
         <Route path="/portfolio" component={Portfolio} />
+        </Switch>
       </Router>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
